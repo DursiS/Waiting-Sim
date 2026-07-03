04b6f6af859e216ce45875754dccfc79183c3b28
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-02 20:07:35 -0400

    Representative CA Interface and Adapters layer for now with __init__ as facade

---
+++ python/Adapters/Converter.py
@@ -0,0 +1,19 @@
+class Controller:
+
+    def trial(self):
+        pass
+
+    def simulation(self, n: int):
+        pass
+
+    def play(self, name: str) -> None:
+        """Start a single manual trial of a player named <name>"""
+        pass
+
+
+class Presenter:
+    pass
+
+
+class ViewModel:
+    pass
