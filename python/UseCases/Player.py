9742099ed7da971896759edb52bf9d4bfe8f9529
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-02 20:06:21 -0400

    added randomized player id to later identifed trials too

--- python/UseCases/Player.py
+++ python/UseCases/Player.py
@@ -1,3 +1,4 @@
+import random
 from datetime import timedelta
 from Entities import Station
 
@@ -6,14 +7,18 @@ class Player:
     """The player travelling the world station-to-station."""
 
     name: str
+    id: int
     station_name: str
     station_id: int
     time_waited: timedelta
-    stations_visited: dict[str, bool]
+    stations_visited: dict[str, bool]Pla
 
-    def __init__(self, name: str, starting_station: Station) -> None:
+    def __init__(
+        self, name: str, starting_station: Station = Station("Coinflip Cove")
+    ) -> None:
         self.name = name
         self.station_name = starting_station.get_name()
         self.station_id = starting_station.get_id()
         self.time_waited = timedelta(seconds=0)
         self.stations_visited = {}
+        self.id = random.randint(1, 10**5)
