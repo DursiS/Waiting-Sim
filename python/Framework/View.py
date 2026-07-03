54bbb0141712f3c9b5f17192d6214790ecce35e6
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-03 16:01:27 -0400

    View and __main__ block skeleton for future workflow

--- python/Framework/View.py
+++ python/Framework/View.py
@@ -1,11 +1,37 @@
 from Adapters import Controller, Presenter
+from Framework import WaitRules
+from UseCases.Interactor import Interactor
 
 
 class View:
+    """The view of the app to hold all GUI logic."""
 
-    def __init__(self, controller: Controller, presenter: Presenter) -> None:
-        pass
+    controller: Controller
+    presenter: Presenter
+    interactor: Interactor
+
+    def __init__(
+        self, controller: Controller, presenter: Presenter, interactor: Interactor
+    ) -> None:
+        self.controller = controller
+        self.presenter = presenter
+        self.interactor = interactor
+
+    def workflow(self):
+        """View methods workflow:
+
+        Controller waits for action
+        -> calls Interactor
+        -> which calls Presenter to display
+        """
+        raise NotImplementedError
+
+
 
 
 if __name__ == "__main__":
-    view = View(Controller(), Presenter())
+    view = View(
+        controller=Controller(),
+        presenter=Presenter(),
+        interactor=Interactor(gateway=WaitRules()),
+    )
