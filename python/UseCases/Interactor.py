afa358af7187fb72e16a89c22f7be15107ce1c10
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-03 16:20:43 -0400

    initial .gitignore

--- python/UseCases/Interactor.py
+++ python/UseCases/Interactor.py
@@ -1,7 +1,22 @@
-from UseCases import RulesGateway
+from Entities import Station
+from UseCases import WaitRulesGateway
+from UseCases.Player import Player
 
 
 class Interactor:
-    rules_gateway: RulesGateway
+    gateway: WaitRulesGateway
 
-    def new_player(self):
+    def __init__(self, gateway: WaitRulesGateway) -> None:
+        self.gateway = gateway
+
+    def new_game(self, name: str, start: Station = Station("Coinflip Cove")) -> None:
+        """Orchestrate a single game."""
+        new_player = Player(name=name, starting_station=start)
+        wait_times = self.station_wait_times(start)
+
+    def station_wait_times(self, station: Station) -> dict[str, float]:
+        """Return a dictionary mapping adjacent stations to their wait-time."""
+        result = {}
+        if station["N"]:
+            pass
+        pass
