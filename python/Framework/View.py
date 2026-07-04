9ff74a5ce76f06f887a63dbe1d3d94f599e4ac3b
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-04 14:36:03 -0400

    tk binded key listeners in View for basic functionality

--- python/Framework/View.py
+++ python/Framework/View.py
@@ -2,6 +2,7 @@ from Adapters import Controller, Presenter
 from Framework import WaitRules
 from Framework.Database import DEFAULT_STATIONS
 from UseCases.Interactor import Interactor
+import tkinter as tk
 
 
 class View:
@@ -20,6 +21,12 @@ class View:
         self._interactor = interactor
         self._busy = False
 
+        self.root = tk.Tk()
+        self.root.bind("p", self.on_play_selected)
+        # self.root.bind("t", self.on_trial_selected)
+        # self.root.bind("s", self.on_simulation_selected)
+        self.root.mainloop()
+
     def workflow(self):
         """View methods workflow:
 
@@ -29,7 +36,7 @@ class View:
         """
         raise NotImplementedError
 
-    def on_play_selected(self) -> None:  # action listener
+    def on_play_selected(self, event: tk.Event) -> None:  # action listener
         """Action Listener to play"""
         if self._busy:
             return
@@ -39,30 +46,32 @@ class View:
         finally:
             self._busy = False
 
-    def on_trial_selected(self) -> None:
-        """Action Listener for trial"""
-        if self._busy:
-            return
-        self._busy = True
-        try:
-            self._controller.handle_trial()
-        finally:
-            self._busy = False
-
-    def on_simulation_selected(self) -> None:
-        """Action Listener for simulation"""
-        if self._busy:
-            return
-        self._busy = True
-        try:
-            self._controller.handle_simulation()
-        finally:
-            self._busy = False
+    # def on_trial_selected(self, event: tk.Event) -> None:
+    #     """Action Listener for trial"""
+    #     if self._busy:
+    #         return
+    #     self._busy = True
+    #     try:
+    #         self._controller.handle_trial()
+    #     finally:
+    #         self._busy = False
+    #
+    # def on_simulation_selected(self, event: tk.Event) -> None:
+    #     """Action Listener for simulation"""
+    #     if self._busy:
+    #         return
+    #     self._busy = True
+    #     try:
+    #         self._controller.handle_simulation()
+    #     finally:
+    #         self._busy = False
 
 
 if __name__ == "__main__":
+
     view = View(
         controller=Controller(),
         presenter=Presenter(),
         interactor=Interactor(gateway=WaitRules(DEFAULT_STATIONS)),
     )
+    # Nothing past this line will run until the app exits, keep above
