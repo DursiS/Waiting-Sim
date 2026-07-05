b5f28add4fda738eb5b941c0a016df44b8b67fde
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-05 16:13:08 -0400

    Player move and wait implementations

--- python/UseCases/Player.py
+++ python/UseCases/Player.py
@@ -1,4 +1,5 @@
 import random
+import time
 from datetime import timedelta
 from Entities import Station
 
@@ -11,10 +12,12 @@ class Player:
     station_name: str
     station_id: int
     time_waited: timedelta
-    stations_visited: dict[str, bool]
+    stations_visited: dict[int, bool]
 
     def __init__(
-        self, name: str, starting_station: Station = Station("Coinflip Cove")
+        self,
+        name: str = "Player1",
+        starting_station: Station = Station("Coinflip Cove"),
     ) -> None:
         self.name = name
         self.station_name = starting_station.get_name()
@@ -22,3 +25,35 @@ class Player:
         self.time_waited = timedelta(seconds=0)
         self.stations_visited = {}
         self.id = random.randint(1, 10**5)
+
+    def move(self, new_station: Station, direction: str) -> bool:
+        """Change the current station of this player, return True
+        if the direction and station was valid."""
+        if direction not in ["N", "S", "W", "E"]:
+            return False
+        elif new_station.id == self.station_id:
+            return False
+        else:
+            self.station_name = new_station.name
+            self.station_id = new_station.id
+            self.stations_visited[new_station.id] = True
+            return True
+
+    def wait(
+        self,
+        t_N: timedelta = None,
+        t_S: timedelta = None,
+        t_E: timedelta = None,
+        t_W: timedelta = None,
+    ) -> None:
+        """Make the user wait for all the transportation to arrive."""
+        times = []
+        if t_N is not None:
+            times.append(t_N)
+        if t_S is not None:
+            times.append(t_S)
+        if t_E is not None:
+            times.append(t_E)
+        if t_W is not None:
+            times.append(t_W)
+        time.sleep(max(times))
