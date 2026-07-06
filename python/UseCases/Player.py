f83d42740c645e1de476149010788e23003d2ec8
Sean Dursi <seanrobertdursi@gmail.com>
2026-07-06 18:32:45 -0400

    implemented move with valid station checking

--- python/UseCases/Player.py
+++ python/UseCases/Player.py
@@ -9,8 +9,7 @@ class Player:
 
     name: str
     id: int
-    station_name: str
-    station_id: int
+    station: Station
     time_waited: timedelta
     stations_visited: dict[int, bool]
 
@@ -20,40 +19,25 @@ class Player:
         starting_station: Station = Station("Coinflip Cove"),
     ) -> None:
         self.name = name
-        self.station_name = starting_station.get_name()
-        self.station_id = starting_station.get_id()
+        self.station = starting_station
         self.time_waited = timedelta(seconds=0)
         self.stations_visited = {}
         self.id = random.randint(1, 10**5)
 
-    def move(self, new_station: Station, direction: str) -> bool:
+    def move(self, new_station: Station) -> bool:
         """Change the current station of this player, return True
         if the direction and station was valid."""
-        if direction not in ["N", "S", "W", "E"]:
+        if new_station not in self.station.get_adjacent_station_ids():
             return False
-        elif new_station.id == self.station_id:
-            return False
-        else:
-            self.station_name = new_station.name
-            self.station_id = new_station.id
-            self.stations_visited[new_station.id] = True
-            return True
 
-    def wait(
-        self,
-        t_N: timedelta = None,
-        t_S: timedelta = None,
-        t_E: timedelta = None,
-        t_W: timedelta = None,
-    ) -> None:
+        self.station = new_station
+        self.stations_visited[new_station.id] = True
+        return True
+
+    def wait(self, times: list[float]) -> timedelta:
         """Make the user wait for all the transportation to arrive."""
-        times = []
-        if t_N is not None:
-            times.append(t_N)
-        if t_S is not None:
-            times.append(t_S)
-        if t_E is not None:
-            times.append(t_E)
-        if t_W is not None:
-            times.append(t_W)
-        time.sleep(max(times))
+        deltas = [timedelta(seconds=t) for t in times]
+        longest = max(deltas)
+        time.sleep(longest.total_seconds())
+        self.time_waited += longest
+        return longest
