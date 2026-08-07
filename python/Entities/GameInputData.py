877ebb7e6353cb9b89739edb27d417e6d3f46fd9
Sean Dursi <seanrobertdursi@gmail.com>
2026-08-07 12:58:35 -0400

    New Output and Input data objects to adhere to CA.

---
+++ python/Entities/GameInputData.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from Entities import Player
+
+
+@dataclass
+class GameInputData:
+    """Configurations for how the game should run."""
+
+    name: str
+    map_id: int
+    rand_arrival: bool
+    gamble: bool
+    raw_bets: list | None
+    animate: bool = True
---
+++ python/Entities/GameOutputData.py
@@ -0,0 +1,22 @@
+from Entities.TurnResult import TurnResult
+
+
+class GameOutputData:
+    """The result of one Game."""
+
+    phase_id: int
+    _turn_results: list[TurnResult]
+    gamble: bool
+    rand_arrival: bool
+    payout: float = 0.0
+
+    def __init__(self, phase_id, turn_results, gamble, rand_arrival, payout) -> None:
+        self.phase_id = phase_id
+        self._turn_results = turn_results
+        self.gamble = gamble
+        self.rand_arrival = rand_arrival
+        self.payout = payout
+
+    def get_results(self) -> list[TurnResult]:
+        """Return the history of turns and what happened on them."""
+        return self._turn_results
